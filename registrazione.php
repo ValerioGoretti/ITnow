0d15fa8ebe484b50c7916d9522a88069f14f0951
DaveBasile09 <64369787+DaveBasile09@users.noreply.github.com>
2020-05-11 18:57:20 +0200

    11/05/2020
    -registrazione diventa php
    -registrazione fa update dei json ad ogni apertura
    -registrazione dei docenti attiva

--- registrazione.php
+++ registrazione.php
@@ -1,4 +1,10 @@
-<!DOCTYPE html>
+<?php
+    include "updateDocentiJson.php";
+    include "updateStudentiJson.php";
+    run();
+    run2();
+                    
+?>
 <html>
     <head>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -9,9 +15,12 @@
         
         <script type="text/javascript"  language="javascript">
             
+            function validaRegistrazione2()
+            {
+                    return valida2();
+            }
             function validaRegistrazione()
             {
- 
                 
                 return valida();
             
@@ -37,6 +46,37 @@
                    
                    if(risultato[i]["mail"]==mail) {alert("La mail inserita non è valida o già utilizzata");return false;}  
                     if((risultato[i]["matricola"]==matricola)||(!isnum)){alert("Matricola non valida o già esistente");return false;}
+                    if(p1!=p2){alert("Le due password inserite sono differenti");return false;}
+
+
+                    
+                }
+                return true;
+
+ 
+            
+                
+            }
+            function valida2()
+            {
+                var request = new XMLHttpRequest();
+                request.open("GET", "dati_docenti.json", false);
+                request.send(null)
+                var risultato = JSON.parse(request.responseText);
+                var mail=document.getElementById("emailStudente2").value;
+                  
+                var p1=document.getElementById("p12").value;
+                var p2=document.getElementById("p22").value;
+                
+                
+          
+                for (i=0;i<risultato.length;i++)
+                {
+                    
+                   
+                   
+                   if(risultato[i]["mail"]==mail) {alert("La mail inserita non è valida o già utilizzata");return false;}  
+                   
                     if(p1!=p2){alert("Le due password inserite sono differenti");return false;}
 
 
@@ -80,26 +120,26 @@
                 
                 <div class="col-md-6">
                     <div class="form-group" >
-                        <input name="nome" type="text" class="form-control-registrazione" placeholder="Nome" value="" required/>
+                        <input required name="nome" type="text" class="form-control-registrazione" placeholder="Nome" value="" required/>
                     </div>
                     <div class="form-group">
-                        <input name="cognome" type="text" class="form-control-registrazione" placeholder="Cognome" value="" required/>
+                        <input required name="cognome" type="text" class="form-control-registrazione" placeholder="Cognome" value="" required/>
                     </div>
                     <div class="form-group">
-                        <input name="email" id="emailStudente" type="email" class="form-control-registrazione" placeholder="Email " required />
+                        <input required name="email" id="emailStudente" type="email" class="form-control-registrazione" placeholder="Email " required />
                         
                     </div>                    
                 </div>
                 
                 <div class="col-md-6">
                     <div class="form-group">
-                        <input name="matricola"id="matricola" type="text"  class="form-control-registrazione" placeholder="Matricola"  required/>
+                        <input  name="matricola"id="matricola" maxlength="7"  type="text"  class="form-control-registrazione" placeholder="Matricola"  required/>
                     </div>
                     <div class="form-group">
-                        <input name="password" type="password" id="p1" class="form-control-registrazione" placeholder="Password " value="" required/>
+                        <input  name="password" minlength="6" type="password" id="p1" class="form-control-registrazione" placeholder="Password " value="" required/>
                     </div>
                     <div class="form-group">
-                        <input type="password" id="p2" class="form-control-registrazione" placeholder="Conferma Password " value="" required/>
+                        <input type="password" minlength="6" id="p2" class="form-control-registrazione" placeholder="Conferma Password " value="" required/>
                     </div>
 
   
@@ -114,48 +154,38 @@
 
             <br>
             
-            <div class="container_registrazione ">
+            <div class="container_registrazione"id="regDocenti" name="regDocenti">
                 <h1 class="t font-weight-bold"  style="color: #822433;">Registrazione Docenti</h1>
+                <p class="t font-weight-bold">La registrazione dei docenti sarà sottoposta ad azioni di convalidazione da parte della moderazione del portale.</p>
                 <br>
                 <br>
-                <form name="regDocenti"class="row register-form ">
+                <form class="row register-form " action="rispondi_registrazione_docenti.php" onsubmit="return validaRegistrazione2();" method="GET">
                     <div class="col-md-6">
                         <div class="form-group" >
-                            <input type="text" class="form-control-registrazione" placeholder="First Name *" value=""/>
+                            <input required name="nome2" id="nome2" type="text" class="form-control-registrazione" placeholder="Nome" value=""/>
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-control-registrazione" placeholder="Last Name *" value="" />
+                            <input required name="cognome2" id="cognome2" type="text" class="form-control-registrazione" placeholder="Cognome" value="" />
                         </div>
                         <div class="form-group">
-                            <input name="email2" type="email" class="form-control-registrazione" placeholder="Email *" value="" />
+                            <input required name="email2" id="emailStudente2" type="email" class="form-control-registrazione" placeholder="Email" value="" />
                         </div>
-                        <div class="form-group">
-                            <input type="text" maxlength="10" minlength="10" class="form-control-registrazione" placeholder="Phone *" value="" />
-                        </div>
+ 
     
     
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            <input type="password" class="form-control-registrazione" placeholder="Password *" value="" />
+                            <input required name="p12" id="p12" minlength="6" type="password" class="form-control-registrazione" placeholder="Password " value="" />
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control-registrazione" placeholder="Confirm Password *" value="" />
+                            <input required minlength="6" name="p22" id="p22" type="password" class="form-control-registrazione" placeholder="Conferma Password " value="" />
                         </div>
+                        
+
                         <div class="form-group">
-                            <select class="form-control-registrazione" >
-                                
-                                
-                                <option class="hidden"  selected disabled>Domanda di sicurezza</option>
-                                <option>What is your Birthdate?</option>
-                                <option>What is Your old Phone Number</option>
-                                <option>What is your Pet Name?</option>
-                            </select>
+                        <input  type="submit" style="padding: 10px;border-radius: 5px;" class="btnRegister" value="Registrazione" name="doc"/>
                         </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control-registrazione" placeholder="`Answer *" value="" />
-                        <div class="form-group">
-                        <input type="submit" class="btnRegister" value="Registrati" name="reg"/>
                     </div>
                 </form>
                
