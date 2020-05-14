d0a8522b64e9ae21a2f3ead440d9735cd778a953
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-05-14 14:24:46 +0200

    sistemato profilo

--- Profilo.php
+++ Profilo.php
@@ -7,18 +7,47 @@
         <link rel="stylesheet" href="css/registrazione.css">
         <link href="css/profilo.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet"/>
-
+        <link href="css/home.css" rel="stylesheet">
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
         
     </head>
     <body>
+        <div class="header row">
+            <div class="col-2"  style="text-align:center" ><a href="index.html" ><img style="width:130px;height:130px;" src="/LOGHI/logo_con_testo_accanto_bordeaux.png"></img></a></div>
+            <div class="ricerca col">  
+                <form action="profiloCorso.php" method="get" name="corsoform">
+               
+                <!--<input type="text" class="testo" placeholder="cerca qui" id="r">-->
+                <input  type="text" placeholder="cerca un corso" autocomplete="off" list="corso" name="corso" class="testo" id="r" onchange="document.corsoform.submit();"/>
+                
+                <datalist id="corso" style="width:100%">
+                    <?php   
+                            $string=file_get_contents('json/corsi.json', 'r');
+                            $corsi=json_decode($string,true);
+                            $no_dup =[];
+                            foreach($corsi as $c){
+                                if(!in_array($c['nomeCorso'],$no_dup))
+                                {?>
+                                    <option value="<?php echo $c['nomeCorso'] ?>"><?php echo $c['nomeCorso'] ?></option>
+                                    <?php array_push($no_dup,$c['nomeCorso']);
+                            }}?>
+                </datalist>
+                </form>
+            </div>
+            
+            <div class="profile " ><a href="profilo.html" style="text-decoration: none; color: #fff;"><i class="fas fa-user" ></i></a></div>
+        </div>
 
     <div style="width: 100%; height: 700px; background-color: #822433;">
         <!--immagine profilo-->
         <div class="immagini">
+            <div class="leftBar" style="background-color: #ffffff;"> <p>ciao </p></div>
+            <!--
             <div class="circle ">
                 <img src="/assets/img/utente.png" style="width: 60%; margin-top: 50px; margin-left: 20%;">
             </div>
             <div class="btn btn-xl btn-primary2 inserimento" >inserisci immagine</div>  
+            -->
         </div>
             
         <!--riepilogo dati-->
