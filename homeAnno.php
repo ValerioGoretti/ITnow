90e935fcd0b6ae2a4ea1cd5623721a6d0e8e581a
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-05-14 18:14:12 +0200

    14/5
    -sistemato file sui post
    -aggiunto download di un file unico per ogni post (prova.txt della cartella download)

--- homeAnno.php
+++ homeAnno.php
@@ -78,8 +78,20 @@
                                 <div class="autore"><h5><?php echo $p['nomeDoc'] . ' '.$p['cognomeDoc'];?></h5> </div>
                                 <div class="linea"><div class="line"></div></div>
                                 <div class="testoPost"><?php echo $p['testo'];?> </div>
-                                <div class="doc"> <div class="attache"><i class="fas fa-paperclip"></i></div></div>
-                                <div class="data"><div ><?php echo '<p style="background-color:#822433; color:white;">'.$p['timestamp']."</p>";?></div> </div>
+                                <div class="data">
+                                <?php
+                                        $dbconn = pg_connect("host=rogue.db.elephantsql.com port=5432 dbname=xsyvwldl user=xsyvwldl password=3GQ9zjDsifaXMFcQkLPrEdDM2lWiPGev");
+                                        $query= "select post.id as postid, file.id,  file.url
+                                        from post join file on post.id=file.post
+                                        where post.id=$1;";
+                                        $result = pg_query_params($dbconn,$query,array($p['idpost'])) or die ('Query failed: '.pg_last_error());
+                                        while ($linefile  = pg_fetch_array($result,null,PGSQL_ASSOC)){?>
+                                                <a class="download" href="download/prova.txt" download="prova.txt"><div class="attache"><?php echo $linefile['url'] ?> <i class="fas fa-paperclip"></i></div></a>
+                                      <?php 
+                                        }?>
+                                </div>
+                                
+                                <div class="doc"><div ><?php echo '<p style="background-color:#822433; color:white;">'.$p['timestamp']."</p>";?></div> </div>
                             </div>
                                 <?php }} ?>
             </div>
