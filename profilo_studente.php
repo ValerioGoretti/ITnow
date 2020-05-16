4a2720d37fa324c1c833e3fc434ff0d49e60cc0b
DaveBasile09 <64369787+DaveBasile09@users.noreply.github.com>
2020-05-16 21:04:52 +0200

    Utilizzo di vue
    -profilo studente funzionante
    -creazione script php per la scrittura js/vue

---
+++ profilo_studente.php
@@ -0,0 +1,38 @@
+<?php
+    
+
+    
+    $matricola=$_SESSION['matricola'];
+    $dbconn = pg_connect("host=rogue.db.elephantsql.com port=5432 dbname=xsyvwldl user=xsyvwldl password=3GQ9zjDsifaXMFcQkLPrEdDM2lWiPGev");
+    $query= "SELECT * FROM studente where matricola='$matricola'";
+    $result = pg_query($dbconn,$query) or die ('Query failed: '.pg_last_error());
+    $nome;
+    $cognome;
+    $matricola;
+    $email;
+    while ($line  = pg_fetch_array($result,null,PGSQL_ASSOC))
+{
+    
+    $nome=$line['nome'];
+    $cognome=$line['cognome'];
+    $matricola=$line['matricola'];
+    $email=$line['email'];
+}
+?>
+
+<script>
+    var app=new Vue(
+        {
+            el:'#app',
+            data:{
+                    nome:'<?php echo $nome?>',
+                    cognome:'<?php echo $cognome?>',
+                    email:'<?php echo $email?>',
+                    matricola:'<?php echo $matricola?>'
+                
+            }
+
+        }
+    );
+</script>
+ 
