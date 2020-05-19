faa4d66e9064908bf920bd8ac6ffc85dce3f64f1
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-05-19 19:28:53 +0200

    NON FUNZIONA UN CAZZO

--- Home.php
+++ Home.php
@@ -45,47 +45,15 @@
         <?php include 'leftBar.php' ?>
         
         <div class="rightBar">barra dx</div>
-        <div class="core ">
-            <div class="spazioPost">
-            
-            <?php       
-                        include "updatePost.php";
-                        $matricola=($_SESSION['matricola']);
-                        run($matricola);
-                        $string=file_get_contents('json/post.json', 'r');
-                        if(!$string){
-                            echo '<div class="containerErrore">';
-                            echo'<p style="font-size:20px;" class="font-weight-light">Al momento non ci sono post da mostrare. Per vedere nuovi post, inizia a seguire dei corsi .</p>';
-                            echo '</div>';
-                        }
-                        else{
-                        $post=json_decode($string,true);
-                        
-                        foreach ($post as $p){?>
-                            <div class="post">
-                                <div class="titolo"> <h3><?php echo $p['titolo'];?></h3> </div>
-                                <div class="autore"><h5><?php echo $p['nomeDoc'] . ' '.$p['cognomeDoc'].'<br>'.$p['corso'].' '.$p['anno'];?></h5> </div>
-                                <div class="linea"><div class="line"></div></div>
-                                <div class="testoPost t font-weight"><?php echo $p['testo'];?> </div>
-                                
-                                <div class="data">
-                                <?php
-                                        $dbconn = pg_connect("host=rogue.db.elephantsql.com port=5432 dbname=xsyvwldl user=xsyvwldl password=3GQ9zjDsifaXMFcQkLPrEdDM2lWiPGev");
-                                        $query= "select post.id as postid, file.id,  file.url
-                                        from post join file on post.id=file.post
-                                        where post.id=$1;";
-                                        $result = pg_query_params($dbconn,$query,array($p['idpost'])) or die ('Query failed: '.pg_last_error());
-                                        while ($linefile  = pg_fetch_array($result,null,PGSQL_ASSOC)){ ?>
-                                                <a href="download/prova.txt" download="prova.txt"><div class="attache"><?php echo $linefile['url'] ?> <i class="fas fa-paperclip"></i></div></a>
-                                      <?php 
-                                        }?>
-                                </div>
-                                <div class="doc"><?php echo $p['data'];?></div>
-                            </div>
-                                <?php }} ?>
-                                
-            </div>
-        </div>   
+        <div class="core">
+            <?php 
+                if($_SESSION['ruolo']=='docente')
+                    include 'pannelloDocente.php'; 
+                if($_SESSION['ruolo']=='studente')
+                    include 'pannelloStudente.php';
+                
+            ?>
+        </div>
         
     </body>
 </html>
--- PannelloDocente.php
+++ PannelloDocente.php
@@ -1,36 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link href="css/home.css" rel="stylesheet">
-        <title></title>
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-        <!-- Third party plugin CSS-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-        
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-        <script>
-            
-           
-
-            $("document").ready(function(){
-                $('#files').hide();             
-            });
-            
-        </script>
-</head>
-
-<body>
-    <div class="core">
     <?php
         $dbconn = pg_connect("host=rogue.db.elephantsql.com port=5432 dbname=xsyvwldl user=xsyvwldl password=3GQ9zjDsifaXMFcQkLPrEdDM2lWiPGev");
         $email=$_SESSION['email'];
@@ -89,7 +56,3 @@
 	}
 	</script>
         
-    
-    
-</body>
-</html>
