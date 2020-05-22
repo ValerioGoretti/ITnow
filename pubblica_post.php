8d901b3e192053fbd09985f338deecc416a8e487
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2020-05-22 19:31:21 +0200

    pusho per pullare

--- pubblica_post.php
+++ pubblica_post.php
@@ -1,5 +1,5 @@
 <html>
-    <head>
+<head>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -7,47 +7,70 @@
         <link href="css/registrazione.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
 
-    </head>
-    <body >
+</head>
+<body >
                 <?php
                     
-                    //include "updateDocentiJson.php";                    
-                    if(isset($_GET['pubblica'])){
-                        $corso=$_GET['corso'];
-                        $titolo=$_GET['titolo'];
-                        $testo=$_GET['testo'];
-                        $file=$_GET['file']; 
-                    
-        
-            
+                    //include "updateDocentiJson.php";             
+                    if(isset($_POST['pubblica'])){   
+                        $corso=$_POST['corso'];
+                        $titolo=$_POST['titolo'];
+                        $testo=$_POST['testo'];
+                        $email=$_SESSION['email'];
+                        echo $corso;
                         $dbconn = pg_connect("host=rogue.db.elephantsql.com port=5432 dbname=xsyvwldl user=xsyvwldl password=3GQ9zjDsifaXMFcQkLPrEdDM2lWiPGev");
-    
-                        $array=array('corso'=>$corso,'titolo'=>$titolo,'testo'=>$testo,'file'=>$file);
-    
-                        $result = pg_insert($dbconn,'post',$array) or die ('Query failed: '.pg_last_error());
                         
-                        //devo inserire nella tabella dei file i file e collgarli con l'id
-                        if($result) 
-                            {}
-                        else{                                
-                        } 
-                        pg_close($dbconn);
+                        $array=array('corso'=>$corso,'titolo'=>$titolo,'testo'=>$testo,'file'=>$file);
+                        $query= "INSERT INTO public.post(
+                            intestazione, testo, anno, docente)
+                            VALUES ($titolo, $testo, $corso, $email )
+                            RETURNING id;";
+                        $result = pg_query($dbconn,$query) or die ('Query failed: '.pg_last_error());
+                        while ($line  = pg_fetch_array($result,null,PGSQL_ASSOC))
+                        {
+                          $id_post=$line['id'];
+                        }
+
+
+                        echo $id_post;
+
+
+
+
+
+
+                        $uploadDir = 'download';
+
+                        function rearrange($files)
+                        {
+                            foreach($files as $key1 => $val1) {
+                                foreach($val1 as $key2 => $val2) {
+                                    for ($i = 0, $count = count($val2); $i < $count; $i++) {
+                                        $newFiles[$i][$key2] = $val2[$i];
+                                    }
+                                }
+                            }
+                            return $newFiles;
+                        }
+                        
+                        $files = rearrange($_FILES);
+
+                        foreach ($files as $file) {
+                            if (UPLOAD_ERR_OK === $file['error']) {
+                                $fileName = basename($file['name']);
+                                echo $fileName;
+                                move_uploaded_file($file['tmp_name'], $uploadDir.DIRECTORY_SEPARATOR.$fileName);
+                            }
+                        }
+
+                        
+                        
                     }
-                    
-                    
-                ?>
-                 
-                  <a class="row" style="color:white; width:fit-content; padding: 8px; border-radius: 6px; background-color:#822433; margin:0 auto; margin-top: 50px;"href="index.html#login">Torna al login</a>
-                    
-                </div>
-                  
+                        
+                       
                 
-
-            </div>
-            
-        </div>
-        </div>
-
+                ?>
+                
     
     </body>
 </html>
