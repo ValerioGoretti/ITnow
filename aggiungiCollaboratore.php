a834017b955effd6ee25a019af7c77ae5501d18f
DaveBasile09 <64369787+DaveBasile09@users.noreply.github.com>
2020-05-27 20:30:21 +0200

    rightBar AnnoDocente
    .

--- aggiungiCollaboratore.php
+++ aggiungiCollaboratore.php
@@ -2,10 +2,11 @@
     $corso = $_GET['corso'];
     $doc=$_GET['collaboratore'];
     $dbconn = pg_connect("host=rogue.db.elephantsql.com port=5432 dbname=xsyvwldl user=xsyvwldl password=3GQ9zjDsifaXMFcQkLPrEdDM2lWiPGev");
-   
+    error_reporting(E_ERROR | E_PARSE);
     $array4=array('anno_didattico'=>$corso,'email'=>$doc);
-    $result4=pg_insert($dbconn,'collaboratori',$array4) or die ('Query failed: '.pg_last_error());
-    
+    $result4=pg_insert($dbconn,'collaboratori',$array4) ;
+    if ($result4) {echo 'Collaboratore inserito correttamente';}
+    else{echo 'Non è stato possibile inserire il collaboratore';}
     
     
 
